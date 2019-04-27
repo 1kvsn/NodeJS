943db559d5b5f7080c55e0dba9ed6441eec87a39
Sasikant <nairs999@gmail.com>
2019-04-27 15:21:43 +0530

    Add userController and verify all functions of the bookStore app.

--- bookStore/routes/users.js
+++ bookStore/routes/users.js
@@ -1,47 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
-var User = require('../models/User');
+var users_controller = require('../controllers/userscontroller');
 
-router.get('/register', (req, res) => {
-	res.render('user')
-})
+//renders register form
+router.get('/register', users_controller.registerForm);
 
-router.post('/', (req, res, next) => {
-	User.create(req.body, (err, user) => {
-		if(err) return res.redirect('/users/register');
-		// console.log(user, '.............user created here');
-		res.redirect('/users/login');
-	})
-});
+//create User upon form submission
+router.post('/', users_controller.createUser);
 
-router.get('/login', (req, res) => {
-	res.render('login')
-})
+//User Login Form
+router.get('/login', users_controller.userLoginForm);
 
-router.post('/login', (req, res, next) => {
-	var {email, password} = req.body;
-	User.findOne({email: email}, (err, user) => {
-		console.log(err, user)
-		if(err) return  res.status(500).redirect('/users/login');
-		if(!user) return res.status(400).send('User NOT found. Please try again!');
-		user.comparePassword(password, (err, isMatch) => {
-			console.log(err, isMatch)
-			if(err) return res.status(500).next(err);
-			if(!isMatch) return res.status(400).send('Incorrect Password. Please try again!')
-			console.log('login success');
-			req.session.userId = user._id;
-			res.redirect('/');
-		})
-	})
-})
+//Authenticate User using bcrypt
+router.post('/login', users_controller.authenticateUser);
 
-router.get('/logout', (req, res) => {
-	req.session.destroy(function(err) {
-		if(err) return next(err);
-		res.redirect('/');
-	})
-})
-
+//Logs out the user
+router.get('/logout', users_controller.logout);
 
 module.exports = router;
